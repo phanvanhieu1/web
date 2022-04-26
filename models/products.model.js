36ff12f801cfa2ca167b5081ecf91ead6e3c637f
phanvanhieu1 <phanvanhieu12a2@gmail.com>
2022-04-26 20:57:49 +0700

    webthuexe

---
+++ models/products.model.js
@@ -0,0 +1,32 @@
+// models/user.model.js
+// load những thư viện chúng ta cần
+var mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);  // plugin slug
+
+// định nghĩ cấu trúc user model
+var Schema = mongoose.Schema;
+var products = new Schema({
+   
+    image: {type: String, required: true},
+    name: {type: String, required: true},
+    cateID: {type: String, required: false},
+    quantity: {type: String, required: true},
+    note: {type: String, required: true},
+    price: {type: String, required: true},//giá
+    date: {type: Date},
+    //chi tiết
+    slug: { type: String, slug: 'name', unique: true },
+    videoid: { type: String, maxlength: 200 },
+    height: { type: String, maxlength: 200 },
+    weight: { type: String, maxlength: 200 },
+    color: { type: String, maxlength: 200 },
+    engine: { type: String, maxlength: 200 },
+    wattage: { type: String, maxlength: 200 },
+    capacity: { type: String, maxlength: 200 },
+    seat: { type: String, maxlength: 200 },
+    
+
+}); 
+
+module.exports = mongoose.model('products', products);
